85af780a0d0f73c72cc0777387c8826dd241070c
EbraheemAnaam <143824387+EbraheemAnaam@users.noreply.github.com>
2025-05-21 11:33:44 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,13 @@
 const cacheName = 'ps4hen-v1';
 const filesToCache = [
   '/ps4hen/',
-  '/ps4hen/index.html',
+  '/ps4hen/main.html',
   '/ps4hen/int64.js',
   '/ps4hen/rop.js',
   '/ps4hen/kexploit.js',
   '/ps4hen/webkit.js',
   '/ps4hen/bg.png'
 ];
-
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(cacheName).then(cache => cache.addAll(filesToCache))
